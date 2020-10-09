1bdae2a61e6b5b47eec356f95d8e2c89ab0a823f
Nirmalraj Joseph <nirmalraj.joseph86@gmail.com>
2020-10-09 21:58:40 +0530

    first commit

---
+++ src/components/Social.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Social =()=> {
+        return (
+            <div class="social">
+            <div class="btnsame btn1">
+				<a href="https://github.com/vikalp2502" target="_blank"><i class="fa fa-github"></i></a>
+			</div>
+			<div class="btnsame btn2">
+				<a href="https://twitter.com/@kaushik_vikalp" target="_blank"><i class="fa fa-twitter"></i></a>
+			</div>
+			<div class="btnsame btn3">
+				<a href="https://linkedin.com/in/vikalp-kaushik-99966a1ab" target="_blank"><i class="fa fa-linkedin"></i></a>
+			</div>
+            </div>
+            )
+        }
+    
+    
+export default Social
+    
