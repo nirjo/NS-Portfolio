4081ece92c5a58a16b9677c1c76222a3334449a9
Nirmalraj Joseph <nirmalraj.joseph86@gmail.com>
2020-10-09 22:59:14 +0530

    first commit

---
+++ build/precache-manifest.ab1316edf53882ea6fd6fbdfc0295bb3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ddf02e8cfc14408ce18ac6d51c9c2a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca8c51d01080c2efd74a",
+    "url": "/static/css/main.e6bfbe08.chunk.css"
+  },
+  {
+    "revision": "b0f5add3d55f807ddaa5",
+    "url": "/static/js/2.c20dbb66.chunk.js"
+  },
+  {
+    "revision": "b688c54f9855505f47aa85b5f9c54246",
+    "url": "/static/js/2.c20dbb66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8c51d01080c2efd74a",
+    "url": "/static/js/main.7342a505.chunk.js"
+  },
+  {
+    "revision": "2903eb1f4c21d3b2e2e5",
+    "url": "/static/js/runtime-main.89ba0b7d.js"
+  },
+  {
+    "revision": "226c30172a0b3d43e93c047dd0cbcdb0",
+    "url": "/static/media/map.226c3017.png"
+  },
+  {
+    "revision": "e1613e3e5beacafffe2dd9e8037dfc44",
+    "url": "/static/media/project1.e1613e3e.png"
+  },
+  {
+    "revision": "b2f649e7e652a24067ea8f960bdd80cf",
+    "url": "/static/media/project2.b2f649e7.png"
+  },
+  {
+    "revision": "a3d72ccd0ae21a28d7ccc7e816ab30bc",
+    "url": "/static/media/project3.a3d72ccd.png"
+  },
+  {
+    "revision": "e78d26131b30631ab42b8ebbdcce82cc",
+    "url": "/static/media/project4.e78d2613.png"
+  }
+]);
---
+++ build/precache-manifest.f91932643e676833e5dfdf9a7c2c8d87.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6948e7f5f9d028e25be4d473f269472a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ab60697b941acc514a2",
+    "url": "/static/css/main.e6bfbe08.chunk.css"
+  },
+  {
+    "revision": "6c2506230c0ca613bb78",
+    "url": "/static/js/2.c677e592.chunk.js"
+  },
+  {
+    "revision": "b688c54f9855505f47aa85b5f9c54246",
+    "url": "/static/js/2.c677e592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab60697b941acc514a2",
+    "url": "/static/js/main.6b2398e9.chunk.js"
+  },
+  {
+    "revision": "2903eb1f4c21d3b2e2e5",
+    "url": "/static/js/runtime-main.89ba0b7d.js"
+  },
+  {
+    "revision": "226c30172a0b3d43e93c047dd0cbcdb0",
+    "url": "/static/media/map.226c3017.png"
+  },
+  {
+    "revision": "e1613e3e5beacafffe2dd9e8037dfc44",
+    "url": "/static/media/project1.e1613e3e.png"
+  },
+  {
+    "revision": "b2f649e7e652a24067ea8f960bdd80cf",
+    "url": "/static/media/project2.b2f649e7.png"
+  },
+  {
+    "revision": "a3d72ccd0ae21a28d7ccc7e816ab30bc",
+    "url": "/static/media/project3.a3d72ccd.png"
+  },
+  {
+    "revision": "e78d26131b30631ab42b8ebbdcce82cc",
+    "url": "/static/media/project4.e78d2613.png"
+  }
+]);
